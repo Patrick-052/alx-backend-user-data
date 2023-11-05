5bb20ac2db4a240d9dbc92d53f5953493474ec51
Patrick-052 <muiruripatrick052@gmail.com>
2023-11-05 14:33:32 +0300

    Filtering using python regex

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Module implementing different functionalities of logging """
+
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
+    for field in fields:
+        message = re.sub(f'{field}=.+?{separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
