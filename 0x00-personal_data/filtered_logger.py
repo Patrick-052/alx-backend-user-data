83007ae82837184246587cd8e2463c2b1b5a57a1
Patrick-052 <muiruripatrick052@gmail.com>
2023-11-05 15:12:56 +0300

    Update

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -19,46 +19,22 @@ def filter_datum(fields: List[str],
 
 
 class RedactingFormatter(logging.Formatter):
-    """Redacting Formatter class"""
+    """ Redacting Formatter class
+        """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str] = None):
-        """Constructor"""
+        """ Constructor method """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """Function that filters values in incoming log records using
-        filter_datum. Values for fields in fields should be filtered.
-        """
+        """ Function that returns a formatted log message """
         message = super(RedactingFormatter, self).format(record)
-        return filter_datum(
-            fields=self.fields,
-            redaction=self.REDACTION,
-            message=message,
-            separator=self.SEPARATOR,
-        )
-
-# class RedactingFormatter(logging.Formatter):
-#     """ Redacting Formatter class
-#         """
-
-#     REDACTION = "***"
-#     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-#     SEPARATOR = ";"
-
-#     def __init__(self, fields: List[str] = None) -> None:
-#         """ Constructor method """
-#         super(RedactingFormatter, self).__init__(self.FORMAT)
-#         self.fields = fields
-
-#     def format(self, record: logging.LogRecord) -> str:
-#         """ Function that returns a formatted log message """
-#         message = super(RedactingFormatter, self).format(record)
-#         return filter_datum(fields=self.fields,
-#                             redaction=self.REDACTION,
-#                             message=message,
-#                             separator=self.SEPARATOR)
+        return filter_datum(fields=self.fields,
+                            redaction=self.REDACTION,
+                            message=message,
+                            separator=self.SEPARATOR)
