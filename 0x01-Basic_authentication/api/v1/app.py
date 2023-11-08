74d6f385ea3bd3a426766ed5f27ae3f84ae7ed6e
Patrick-052 <muiruripatrick052@gmail.com>
2023-11-08 21:43:14 +0300

    Request validation!

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -2,16 +2,38 @@
 """
 Route module for the API
 """
+import os
 from os import getenv
 from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
-
+from flask import Flask, jsonify, abort, request
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+if getenv('AUTH_TYPE'):
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+
+@app.before_request
+def request_validation():
+    """ Function that validates all requests """
+    if auth is None:
+        return
+    if auth.require_auth(
+            request.path,
+            ['/api/v1/status/',
+             '/api/v1/unauthorized/',
+             '/api/v1/forbidden/']):
+        if auth.authorization_header(request) is None:
+            abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
+    else:
+        return
 
 
 @app.errorhandler(404)
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -21,7 +21,9 @@ class Auth:
     def authorization_header(self, request=None) -> str:
         """ Function that handles authorization header
         """
-        return None
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Function that handles current user
