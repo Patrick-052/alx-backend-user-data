89a82d6126600677c9434ee0631d0d6c8e7293d6
Patrick-052 <muiruripatrick052@gmail.com>
2023-11-12 16:34:08 +0300

    rsync Basic Auth with Session Auth

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" Implementing the API authentication """
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ Authentication class """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Function that implements authentication requirement or not
+        """
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        elif path in excluded_paths or path + '/' in excluded_paths:
+            return False
+        else:
+            return True
+
+    def authorization_header(self, request=None) -> str:
+        """ Function that handles authorization header
+        """
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Function that handles current user
+        """
+        return None
