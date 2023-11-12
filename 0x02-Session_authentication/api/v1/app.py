b5d5136bd006adfb1738aeceadbda89e4d439575
Patrick-052 <muiruripatrick052@gmail.com>
2023-11-12 18:16:20 +0300

    Before request valdation

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -28,12 +28,15 @@ def request_validation():
     """ Function that validates all requests """
     if auth is None:
         return
-    if auth.require_auth(
-            request.path,
-            ['/api/v1/status/',
-             '/api/v1/unauthorized/',
-             '/api/v1/forbidden/']):
-        if auth.authorization_header(request) is None:
+    if auth.require_auth(request.path,
+                         [
+                             '/api/v1/status/',
+                             '/api/v1/unauthorized/',
+                             '/api/v1/forbidden/',
+                             '/api/v1/auth_session/login/'
+                         ]):
+        if auth.authorization_header(request) is None and auth.session_cookie(
+                request) is None:
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
