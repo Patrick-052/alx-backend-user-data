3abe528df5260cea6a74de173a2b16380628a47b
Patrick-052 <muiruripatrick052@gmail.com>
2023-11-12 17:27:53 +0300

    Basic session auth

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Implementing Session Authentication """
+
+from os import getenv
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ Session Authentication class """
+
+    pass
