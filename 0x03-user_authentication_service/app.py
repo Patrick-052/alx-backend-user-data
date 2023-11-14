302e2c91b636683aff20be52c0728872e63a66d5
Patrick-052 <muiruripatrick052@gmail.com>
2023-11-14 23:10:02 +0300

    Registering a user using flask and sqlalchemy

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,14 +1,30 @@
 #!/usr/bin/env python3
 """ Implementing a flask application """
 
-from flask import Flask, jsonify
+from auth import Auth
+from typing import Union, Tuple
+from flask import Flask, jsonify, request
+
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def basic_route() -> str:
     """ Basic route """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> Union[str, Tuple]:
+    try:
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
