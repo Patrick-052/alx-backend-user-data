2b52ac97ef9aed12e617d467ff164118c4562cbc
Patrick-052 <muiruripatrick052@gmail.com>
2023-11-14 21:38:17 +0300

    Update

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -33,14 +33,31 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """ Method that saves a new user to the database """
+        """add user to database
+
+        Args:
+            email (string): email of user
+            hashed_password (string): password of user
+        Returns:
+            User: user created
+        """
         if not email or not hashed_password:
-            return None
-        new_user = User(email=email, hashed_password=hashed_password)
+            return
+        user = User(email=email, hashed_password=hashed_password)
         session = self._session
-        session.add(new_user)
+        session.add(user)
         session.commit()
-        return new_user
+        return user
+
+    # def add_user(self, email: str, hashed_password: str) -> User:
+    #     """ Method that saves a new user to the database """
+    #     if not email or not hashed_password:
+    #         return None
+    #     new_user = User(email=email, hashed_password=hashed_password)
+    #     session = self._session
+    #     session.add(new_user)
+    #     session.commit()
+    #     return new_user
 
     def find_user_by(self, **kwargs) -> User:
         """ Method that returns the first row found in the users table
