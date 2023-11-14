f788a14d15f42f39e200d97aa8cd71d8cea199bd
Patrick-052 <muiruripatrick052@gmail.com>
2023-11-15 00:04:58 +0300

    Update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -32,7 +32,7 @@ class Auth:
         except NoResultFound:
             return False
 
-    def _generate_uuid(self):
+    def _generate_uuid() -> str:
         """ Returns a string representation of a new UUID """
         new_id = str(uuid4())
         return new_id
