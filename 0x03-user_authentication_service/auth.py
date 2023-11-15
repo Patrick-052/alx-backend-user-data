3a1db3b0612c9a03722f516da29ff9007c17784b
Patrick-052 <muiruripatrick052@gmail.com>
2023-11-15 14:34:46 +0300

    Destroying a user session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -58,7 +58,7 @@ class Auth:
             user = self._db.find_user_by(id=user_id)
             self._db.update_user(user_id, session_id=None)
         except NoResultFound:
-            return
+            pass
 
 
 def _generate_uuid() -> str:
