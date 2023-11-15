ed6472d68db20ccc35589400a6f57a168274b4a2
Patrick-052 <muiruripatrick052@gmail.com>
2023-11-15 17:35:19 +0300

    Update password end-point

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -91,12 +91,12 @@ def update_password() -> Union[Tuple[str, int], None]:
     """ View that implements updating passwords """
     email = request.form.get('email')
     reset_token = request.form.get('reset_token')
-    password = request.form.get('new_password')
+    new_password = request.form.get('new_password')
 
     try:
-        AUTH.update_password(reset_token, password)
+        AUTH.update_password(reset_token, new_password)
         return jsonify({"email": email, "message": "Password updated"}), 200
-    except NoResultFound:
+    except Exception:
         abort(403)
 
 
