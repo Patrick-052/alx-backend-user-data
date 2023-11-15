c8a74ab27045eb0ed7c470b75a9c080b79bfc80e
Patrick-052 <muiruripatrick052@gmail.com>
2023-11-15 15:15:53 +0300

    Update

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -48,16 +48,32 @@ def login() -> Union[str, Tuple]:
 
 
 @app.route('/sessions', methods=['DELETE'], strict_slashes=False)
-def logout() -> Union[str, Tuple]:
-    """ View that destroys user's session thus login them out """
+def logout() -> str:
+    """ logout
+
+    Return:
+       str: message
+    """
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
-
     if user:
         AUTH.destroy_session(user.id)
         return redirect('/')
     else:
         abort(403)
+
+
+# @app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+# def logout() -> Union[str, Tuple]:
+#     """ View that destroys user's session thus login them out """
+#     session_id = request.cookies.get('session_id')
+#     user = AUTH.get_user_from_session_id(session_id)
+
+#     if user:
+#         AUTH.destroy_session(user.id)
+#         return redirect('/')
+#     else:
+#         abort(403)
 
 
 if __name__ == "__main__":
